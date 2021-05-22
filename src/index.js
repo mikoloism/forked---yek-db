c315b96df686d5845c12495dd00f33f10b3afc19
mikoloism <mikoloism.github@gmail.com>
2021-05-23 02:50:40 +0430

    Fix/Update exporting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import { Database } from './db.js';
-import { Model } from './model';
-import { refrence } from './refrence';
+import { Database, createDatabase } from './database/db.js';
+import { Model } from './database/model.js';
+import { refrence } from './database/refrence.js';
+import DataType from './database/data-types.js';
 
 const DashDB = Database;
 
-export { DashDB, Database, refrence, Model };
+export { DashDB, Database, createDatabase, refrence, Model, DataType };
